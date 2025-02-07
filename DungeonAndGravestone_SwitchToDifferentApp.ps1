e43ba104c01ebeb896fa72b9218fcf968874fae8
Ayush Mishra <130413472+AyushMishra1603@users.noreply.github.com>
2025-02-07 19:37:59 +0530

    Update DungeonAndGravestone_SwitchToDifferentApp.ps1

--- DungeonAndGravestone_SwitchToDifferentApp.ps1
+++ DungeonAndGravestone_SwitchToDifferentApp.ps1
@@ -1,7 +1,7 @@
 ﻿Function DungeonAndGravestone_SwitchToDifferentApp($launchUsing,$gamePlayTime){
 
   $game_title = "DungeonAndGravestone_SwitchToDifferentApp"
-  $game_title_hub= "Dungeon And Gravestone SwitchToDifferentApp"
+  $game_title_hub= "DungeonAndGravestoneSwitchToDifferentApp"
   $testDescriptionPass = ""
   $testDescriptionFail = ""  
   $tc_status = $false
