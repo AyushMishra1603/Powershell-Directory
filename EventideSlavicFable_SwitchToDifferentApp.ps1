4df53d5c7edc3ea5d1b9436e001775e846b8278a
Ayush Mishra <130413472+AyushMishra1603@users.noreply.github.com>
2025-04-12 22:39:09 +0530

    Update EventideSlavicFable_SwitchToDifferentApp.ps1

--- EventideSlavicFable_SwitchToDifferentApp.ps1
+++ EventideSlavicFable_SwitchToDifferentApp.ps1
@@ -1,7 +1,7 @@
 ﻿Function EventideSlavicFable_SwitchToDifferentApp($launchUsing,$gamePlayTime){
 
   $game_title = "EventideSlavicFable_SwitchToDifferentApp"
-  $game_title_hub= "EventideSlavicFableSwitchToDifferentApp"
+  $game_title_hub= "Eventide Slavic Fable Switch To Different App"
   $testDescriptionPass = ""
   $testDescriptionFail = ""  
   $tc_status = $false
