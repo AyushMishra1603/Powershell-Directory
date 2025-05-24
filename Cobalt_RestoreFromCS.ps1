64f5680ffb7cea14b5ac67e8c0e3543c509d33b8
Ayush Mishra <130413472+AyushMishra1603@users.noreply.github.com>
2025-05-25 03:24:34 +0530

    Update Cobalt_RestoreFromCS.ps1

--- Cobalt_RestoreFromCS.ps1
+++ Cobalt_RestoreFromCS.ps1
@@ -1,7 +1,7 @@
 ﻿Function Cobalt_RestoreFromCS($launchUsing){
 
   $game_title = "Cobalt_RestoreFromCS"
-  $game_title_hub= "Cobalt RestoreFromCS"
+  $game_title_hub= "CobaltRestoreFromCS"
   $testDescriptionPass = ""
   $testDescriptionFail = ""  
   $tc_status = $false
